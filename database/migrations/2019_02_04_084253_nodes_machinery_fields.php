6a22ef242ae773d97f956352ea63d00e87e6c93e
Evgeny Tulikov <etulikov@gmail.com>
2019-02-04 12:14:09 +0300

    Node Machinery DBA

--- database/migrations/2019_02_04_084253_nodes_machinery_fields.php
+++ database/migrations/2019_02_04_084253_nodes_machinery_fields.php
@@ -16,6 +16,16 @@ class NodesMachineryFields extends Migration
         Schema::create('nodes_machinery_fields', function (Blueprint $table) {
             $table->increments('nmf_id');
             $table->integer('node')->unsigned()->default(0);
+            // English names
+            $table->string('nmf_name_en')->nullable();
+            $table->longText('nmf_body_en')->nullable();
+            $table->text('nmf_description_en')->nullable();
+            $table->text('nmf_short_en')->nullable();
+            // Arabic names
+            $table->string('nmf_name_ar')->nullable();
+            $table->longText('nmf_body_ar')->nullable();
+            $table->text('nmf_description_ar')->nullable();
+            $table->text('nmf_short_ar')->nullable();
 
             $table->foreign('node')->references('nid')->on('nodes');
 
