1d521d02880283a96374fdf40bc6a025f2676c85
Evgeny Tulikov <etulikov@gmail.com>
2019-02-05 10:09:47 +0300

    Admin assets

--- resources/views/layouts/secured.blade.php
+++ resources/views/layouts/secured.blade.php
@@ -6,8 +6,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@if($pageTitle && NULL !== $pageTitle) {{ $pageTitle }} @else {{ config('app.name', 'Laravel') }} @endif</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('admin-panel/style.css') }}">
 </head>
-<body>
+    <body>
 
-</body>
+        <!-- Scripts -->
+        <script src="{{ asset('admin-panel/vendor.js') }}"></script>
+        <script src="{{ asset('admin-panel/bundle.js') }}"></script>
+    </body>
 </html>
