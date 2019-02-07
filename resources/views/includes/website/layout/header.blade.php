7a344990fdb28f217279d8d7e25018d6c497b74f
Evgeny Tulikov <etulikov@gmail.com>
2019-02-07 12:54:47 +0300

    Main menu

--- resources/views/includes/website/layout/header.blade.php
+++ resources/views/includes/website/layout/header.blade.php
@@ -10,7 +10,27 @@
             </div>
             <!-- /.col-12 col-lg-6 -->
             <div class="col-12 col-lg-6">
-
+                <div class="d-flex justify-content-end">
+                    <div class="header__top-search">
+                        <form action="#" method="get">
+                            <input type="text" name="q" required />
+                            <button type="submit"><i class="fas fa-search"></i></button>
+                        </form>
+                    </div>
+                    <!-- /.header__top-search -->
+                    <div class="header__top-auth">
+                        <a href="#"><i class="far fa-user"></i> <span>Login</span></a>
+                    </div>
+                    <!-- /.header__top-auth -->
+                    <div class="header__top-lang">
+                        <select name="lang" id="lang">
+                            <option value="en">English</option>
+                            <option value="ar">Arabic</option>
+                        </select>
+                    </div>
+                    <!-- /.header__top-lang -->
+                </div>
+                <!-- /.d-flex justify-content-end -->
             </div>
             <!-- /.col-12 col-lg-6 -->
         </div>
@@ -19,3 +39,49 @@
     <!-- /.container -->
 </div>
 <!-- /.header__top -->
+<div class="shadow pt-3 pb-3 header__main">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-2">
+                <div class="text-center header__main-logo">
+                    <a href="{{ route('homePage') }}"><img src="{{ asset('assets/logos/logo.png') }}" alt="Gararat Logo" height="64"></a>
+                </div>
+                <!-- /.header__main-logo -->
+            </div>
+            <!-- /.col-12 col-lg-2 -->
+            <div class="col-12 col-lg-7">
+                <div class="header__main-menu">
+                    <ul>
+                        <li @if(in_array(\Request::route()->getName(), ['homePage'])) class="active" @endif>
+                            <a href="{{ route('homePage') }}">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Products</a>
+                        </li>
+                        <li>
+                            <a href="#">Parts</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.header__main-menu -->
+            </div>
+            <!-- /.col-12 col-lg-7 -->
+            <div class="col-12 col-lg-3">
+                <div class="header__main-cart">
+                    Cart
+                </div>
+                <!-- /.header__main-cart -->
+            </div>
+            <!-- /.col-12 col-lg-3 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /.shadow header__main -->
