26c3f127d6f2ee0976bc43a5abf895971bf47314
Evgeny Tulikov <etulikov@gmail.com>
2019-02-07 11:49:07 +0300

    Fonts

---
+++ resources/views/includes/website/layout/header.blade.php
@@ -0,0 +1,17 @@
+<div class="header__top">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+            <div class="col-12 col-lg-6">
+
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /.header__top -->
