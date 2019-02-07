af0306266215f0132e05e25d019b5e3cc8d61e5d
Evgeny Tulikov <etulikov@gmail.com>
2019-02-07 11:57:37 +0300

    New Font Awesome and Container

--- resources/views/includes/website/layout/header.blade.php
+++ resources/views/includes/website/layout/header.blade.php
@@ -2,7 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-6">
-
+                <div class="header__top-contacts">
+                    <a href="tel:+375224443333"><i class="fas fa-phone"></i><span>+375-22-444-3333</span></a>
+                    <a href="email:info@gararat.com"><i class="far fa-envelope"></i><span>info@gararat.com</span></a>
+                </div>
+                <!-- /.header__top-contacts -->
             </div>
             <!-- /.col-12 col-lg-6 -->
             <div class="col-12 col-lg-6">
