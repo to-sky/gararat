5f286cdcab8e9f4e347aeb5171c41fa62bd98c0d
Evgeny Tulikov <etulikov@gmail.com>
2019-02-07 15:41:49 +0300

    Finish home page

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -48,6 +48,8 @@
             <div class="row">
                 <div class="col-12 col-lg-7">
                     <h2>Belarus agriculture tractors</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eos eveniet ex exercitationem facilis itaque laudantium nemo quidem, reiciendis soluta totam unde vero vitae! Architecto autem distinctio itaque libero sint!</p>
+                    <p class="mb-0"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dignissimos eaque exercitationem incidunt magnam obcaecati odit officiis porro temporibus veniam. A commodi impedit laborum obcaecati officiis perferendis perspiciatis, recusandae vero!</span><span>Architecto, dignissimos eius eos error facere fugit nemo nulla placeat porro provident, quia quod ullam voluptas! Corporis dicta error itaque minus, non numquam praesentium quasi totam. Et labore pariatur quibusdam.</span></p>
                 </div>
                 <!-- /.col-12 col-lg-6 -->
                 <div class="col-12 col-lg-5">
@@ -56,8 +58,119 @@
                 <!-- /.col-12 col-lg-6 -->
             </div>
             <!-- /.row -->
+            <div class="section__icons">
+                <div class="row">
+                    <div class="text-center col-12 col-md-6 col-lg-3 section__brg"><img src="{{ asset('assets/sections/5.png') }}" alt="530+ Happy customer"><p><strong>530+</strong></p><p>Happy customer</p></div>
+                    <!-- /.text-center col-12 col-md-6 col-lg-3 section__brg -->
+                    <div class="text-center col-12 col-md-6 col-lg-3 section__brg"><img src="{{ asset('assets/sections/6.png') }}" alt="12+ Years of experience"><p><strong>12+</strong></p><p>Years of experience</p></div>
+                    <!-- /.text-center col-12 col-md-6 col-lg-3 section__brg -->
+                    <div class="text-center col-12 col-md-6 col-lg-3 section__brg"><img src="{{ asset('assets/sections/7.png') }}" alt="24 Month warranty"><p><strong>24</strong></p><p>Month warranty</p></div>
+                    <!-- /.text-center col-12 col-md-6 col-lg-3 section__brg -->
+                    <div class="text-center col-12 col-md-6 col-lg-3"><img src="{{ asset('assets/sections/8.png') }}" alt="270+ Projects completed"><p><strong>270+</strong></p><p>Projects completed</p></div>
+                    <!-- /.text-center col-12 col-md-6 col-lg-3 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.section__icons -->
         </div>
         <!-- /.container -->
     </div>
     <!-- /.section -->
+    <div class="section bg-section">
+        <div class="container">
+            <h2>Our News</h2>
+            <div class="section__news">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="shadow-sm section__news-item">
+                            <div class="news-item__image">
+                                <a href="#">
+                                    <img src="{{ asset('assets/sections/2.jpg') }}" alt="News Alt" class="image">
+                                    <div class="news-item__date">
+                                        <h4>12</h4>
+                                        <h6>Nov</h6>
+                                    </div>
+                                    <!-- /.news-item__date -->
+                                </a>
+                            </div>
+                            <!-- /.news-item__image -->
+                            <div class="news-item__body">
+                                <h3><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dignissimos, dolorem facere fuga fugiat illo ipsum magni molestias odit omnis perferendis placeat quaerat ratione recusandae repellendus sequi vel voluptas voluptate?</p>
+                            </div>
+                        </div>
+                        <!-- /.shadow-sm section__news-item -->
+                    </div>
+                    <!-- /.col-12 col-md-6 col-lg-3 -->
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="shadow-sm section__news-item">
+                            <div class="news-item__image">
+                                <a href="#">
+                                    <img src="{{ asset('assets/sections/2.jpg') }}" alt="News Alt" class="image">
+                                    <div class="news-item__date">
+                                        <h4>12</h4>
+                                        <h6>Nov</h6>
+                                    </div>
+                                    <!-- /.news-item__date -->
+                                </a>
+                            </div>
+                            <!-- /.news-item__image -->
+                            <div class="news-item__body">
+                                <h3><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dignissimos, dolorem facere fuga fugiat illo ipsum magni molestias odit omnis perferendis placeat quaerat ratione recusandae repellendus sequi vel voluptas voluptate?</p>
+                            </div>
+                        </div>
+                        <!-- /.shadow-sm section__news-item -->
+                    </div>
+                    <!-- /.col-12 col-md-6 col-lg-3 -->
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="shadow-sm section__news-item">
+                            <div class="news-item__image">
+                                <a href="#">
+                                    <img src="{{ asset('assets/sections/2.jpg') }}" alt="News Alt" class="image">
+                                    <div class="news-item__date">
+                                        <h4>12</h4>
+                                        <h6>Nov</h6>
+                                    </div>
+                                    <!-- /.news-item__date -->
+                                </a>
+                            </div>
+                            <!-- /.news-item__image -->
+                            <div class="news-item__body">
+                                <h3><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dignissimos, dolorem facere fuga fugiat illo ipsum magni molestias odit omnis perferendis placeat quaerat ratione recusandae repellendus sequi vel voluptas voluptate?</p>
+                            </div>
+                        </div>
+                        <!-- /.shadow-sm section__news-item -->
+                    </div>
+                    <!-- /.col-12 col-md-6 col-lg-3 -->
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="shadow-sm section__news-item">
+                            <div class="news-item__image">
+                                <a href="#">
+                                    <img src="{{ asset('assets/sections/2.jpg') }}" alt="News Alt" class="image">
+                                    <div class="news-item__date">
+                                        <h4>12</h4>
+                                        <h6>Nov</h6>
+                                    </div>
+                                    <!-- /.news-item__date -->
+                                </a>
+                            </div>
+                            <!-- /.news-item__image -->
+                            <div class="news-item__body">
+                                <h3><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dignissimos, dolorem facere fuga fugiat illo ipsum magni molestias odit omnis perferendis placeat quaerat ratione recusandae repellendus sequi vel voluptas voluptate?</p>
+                            </div>
+                        </div>
+                        <!-- /.shadow-sm section__news-item -->
+                    </div>
+                    <!-- /.col-12 col-md-6 col-lg-3 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.section__news -->
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.section bg-section -->
 @endsection
