c7bda0b39817b94e862748b0cb3548c7559701b7
Evgeny Tulikov <smilecatwebstudio@gmail.com>
2019-02-18 10:54:44 +0300

    Intervention image

--- resources/views/secured/nodes/equipment/add.blade.php
+++ resources/views/secured/nodes/equipment/add.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.secured')
 
 @section('content')
+    <form action="{{ route('saveNewCatalogItemAPI') }}" method="post" autocomplete="off">
+        @csrf
+        <div class="row">
 
+        </div>
+    </form>
 @endsection
