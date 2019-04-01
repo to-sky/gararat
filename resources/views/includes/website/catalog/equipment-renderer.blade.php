2d1dcafc3505a59eb5045a2ada37863a4c72d3ee
Evgeny Tulikov <smilecatwebstudio@gmail.com>
2019-04-01 13:27:32 +0300

    Fix EQ images in listings

--- resources/views/includes/website/catalog/equipment-renderer.blade.php
+++ resources/views/includes/website/catalog/equipment-renderer.blade.php
@@ -1,8 +1,12 @@
 @foreach($products as $key => $product)
     <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 product__wrapper">
         <div class="text-center shadow-sm product__inner">
-            <a href="{{ route('singleNodePage', $product->nid) }}">
-                <img src="{{ asset($product->thumb_path) }}" alt="{{ $product->n_name_en }}" class="image">
+            <a href="{{ route('singleNodePage', $product->nid) }}" style="margin: 0 auto;">
+                @if($product->has_photo === 0)
+                    <img src="{{ asset('assets/logos/logo.jpg') }}" class="image" alt="{{ $product->n_name_en }}">
+                @else
+                    <img src="{{ asset($product->thumb_path) }}" class="image" alt="{{ $product->n_name_en }}">
+                @endif
             </a>
         </div>
         <!-- /.text-center product__inner -->
