638e75e64867bf0d4d5178e87fbc415300a3cfed
Evgeny Tulikov <smilecatwebstudio@gmail.com>
2019-04-18 07:26:25 +0300

    Extend Editor

--- resources/views/layouts/secured.blade.php
+++ resources/views/layouts/secured.blade.php
@@ -42,9 +42,10 @@
                     toolbar: [
                         // [groupName, [list of button]]
                         ['style', ['bold', 'italic', 'underline', 'clear']],
-                        ['font', ['strikethrough', 'superscript', 'subscript']],
-                        ['para', ['style', 'ul', 'ol', 'paragraph']],
+                        ['font', ['strikethrough', 'superscript', 'subscript', 'fontsize', 'color', 'forecolor', 'backcolor']],
+                        ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
                         ['insert', ['link', 'table', 'picture', 'video', 'hr']],
+                        ['misc', ['fullscreen', 'undo', 'redo', 'help']]
                     ]
                 });
             })(jQuery);
