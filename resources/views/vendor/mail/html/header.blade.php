e2a5b27addec20e3a167cb6db6e8a28638995faf
Dmitry Tolsky <ermolovdima@gmail.com>
2021-10-04 12:06:32 +0300

    fixed email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ SettingService::getLogoUrl('header') }}" class="logo" alt="Gararat">
+    <img src="{{ SettingService::getLogoUrl('header') }}" class="logo" alt="Belmach">
 </a>
 </td>
 </tr>
